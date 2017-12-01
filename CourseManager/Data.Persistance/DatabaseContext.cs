5a43d8ff01297c5ff0b42400f7573d3b55a65fcc
Radu-Cristian Vulpescu <radu.vulpescu16@gmail.com>
2017-12-01 17:54:27 +0200

    Added Nuget packages

--- CourseManager/Data.Persistance/DatabaseContext.cs
+++ CourseManager/Data.Persistance/DatabaseContext.cs
@@ -1,5 +1,4 @@
-﻿using Data.Domain.Entities;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 
 namespace Data.Persistance
 {
---
+++ CourseManager/Data.Persistance/IDatabaseContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Data.Persistance
+{
+    public interface IDatabaseContext
+    {
+
+        int SaveChanges();
+    }
+}
