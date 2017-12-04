e6f098953fd07f9c5f43141987cf8101c583071b
Radu-Cristian Vulpescu <radu.vulpescu16@gmail.com>
2017-12-05 01:41:55 +0200

    Implemented DDD with Data annotations

--- CourseManager/Presentation/Models/UserAccountAdminEditModel.cs
+++ CourseManager/Presentation/Models/UserAccountAdminEditModel.cs
@@ -4,10 +4,18 @@ namespace Presentation.Models
 {
     public class UserAccountAdminEditModel
     {
+        [Required(ErrorMessage = "First name is required.")]
+        [MinLength(1, ErrorMessage = "First name must have at least 1 character.")]
+        [MaxLength(100, ErrorMessage = "First name cannot exceed 100 characters.")]
         public string FirstName { get; set; }
 
+        [Required(ErrorMessage = "Last name is required.")]
+        [MinLength(1, ErrorMessage = "Last name must have at least 1 character.")]
+        [MaxLength(100, ErrorMessage = "Last name cannot exceed 100 characters.")]
         public string LastName { get; set; }
 
+        [Required(ErrorMessage = "E-mail address is required.")]
+        [MaxLength(200, ErrorMessage = "E-mail adress must not exceed 200 charaters.")]
         [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
 
--- CourseManager/Presentation/Models/UserAccountStudentEditModel.cs
+++ CourseManager/Presentation/Models/UserAccountStudentEditModel.cs
@@ -4,14 +4,24 @@ namespace Presentation.Models
 {
     public class UserAccountStudentEditModel
     {
+        [Required(ErrorMessage = "First name is required.")]
+        [MinLength(1, ErrorMessage = "First name must have at least 1 character.")]
+        [MaxLength(100, ErrorMessage = "First name cannot exceed 100 characters.")]
         public string FirstName { get; set; }
 
+        [Required(ErrorMessage = "Last name is required.")]
+        [MinLength(1, ErrorMessage = "Last name must have at least 1 character.")]
+        [MaxLength(100, ErrorMessage = "Last name cannot exceed 100 characters.")]
         public string LastName { get; set; }
 
+        [StringLength(16, ErrorMessage = "Registration number must have 16 characters.")]
         public string RegistrationNumber { get; set; }
 
+        [RegularExpression(@"([AB][1-9])|X")]
         public string Group { get; set; }
 
+        [Required(ErrorMessage = "E-mail address is required.")]
+        [MaxLength(200, ErrorMessage = "E-mail adress must not exceed 200 charaters.")]
         [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
 
