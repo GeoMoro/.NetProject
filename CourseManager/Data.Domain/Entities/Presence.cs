81f5961d1c34e8a1a7f5c1b0357dc94cff87426b
Adrian Dorneanu <adrian.dorneanu96@gmail.com>
2017-12-05 17:32:32 +0200

    Presence Entity and Repository

---
+++ CourseManager/Data.Domain/Entities/Presence.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations;
+using Data.Domain.Interfaces;
+
+namespace Data.Domain.Entities
+{
+    public class Presence
+    {
+        public Presence()
+        {
+            //EF
+        }
+
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required(ErrorMessage = "Laboratory is required")]
+        [MinLength(1, ErrorMessage = "Laboratory must have at least 1 character")]
+        public string Laboratory { get; set; }
+
+        public bool Present { get; set; }
+    }
+}
---
+++ CourseManager/Data.Domain/Interfaces/IPresenceRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using Data.Domain.Entities;
+
+namespace Data.Domain.Interfaces
+{
+    public interface IPresenceRepository
+    {
+        IReadOnlyList<Presence> GetAllPresence();
+        Presence GetPresenceById(Guid id);
+        void CreatePresence(Presence presence);
+        void UpdatePresence(Presence presence);
+        void DeletePresence(Presence presence);
+    }
+}
