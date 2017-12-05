8e1a294cdb8fdb4e4e0389f0c2343c93c45c8c40
Radu-Cristian Vulpescu <radu.vulpescu16@gmail.com>
2017-12-05 02:24:01 +0200

    Add Migration for Updating UserAccounts table

--- CourseManager/Data.Persistance/Migrations/20171205002223_UpdateUserAccounts.Designer.cs
+++ CourseManager/Data.Persistance/Migrations/20171205002223_UpdateUserAccounts.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace Data.Persistance.Migrations
 {
     [DbContext(typeof(DatabaseContext))]
-    partial class DatabaseContextModelSnapshot : ModelSnapshot
+    [Migration("20171205002223_UpdateUserAccounts")]
+    partial class UpdateUserAccounts
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ CourseManager/Data.Persistance/Migrations/20171205002223_UpdateUserAccounts.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace Data.Persistance.Migrations
+{
+    public partial class UpdateUserAccounts : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "RegistrationNumber",
+                table: "UserAccounts",
+                maxLength: 16,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Password",
+                table: "UserAccounts",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LastName",
+                table: "UserAccounts",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FirstName",
+                table: "UserAccounts",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "UserAccounts",
+                maxLength: 200,
+                nullable: false,
+                oldClrType: typeof(string));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "RegistrationNumber",
+                table: "UserAccounts",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 16,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Password",
+                table: "UserAccounts",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 100);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LastName",
+                table: "UserAccounts",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 100);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FirstName",
+                table: "UserAccounts",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 100);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "UserAccounts",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 200);
+        }
+    }
+}
