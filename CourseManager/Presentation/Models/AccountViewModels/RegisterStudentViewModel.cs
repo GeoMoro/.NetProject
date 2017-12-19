2ad8b381baff480b1a05aad03adc52b7a7d8709f
Radu-Cristian Vulpescu <radu.vulpescu16@gmail.com>
2017-12-19 02:30:18 +0200

    Changed the regex for REgistration Number

--- CourseManager/Presentation/Models/AccountViewModels/RegisterStudentViewModel.cs
+++ CourseManager/Presentation/Models/AccountViewModels/RegisterStudentViewModel.cs
@@ -6,16 +6,18 @@ namespace Presentation.Models.AccountViewModels
     {
         [Required]
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 1)]
-        [Display(Name = "First Name")]
+        //[RegularExpression(@"[A-Z][a-z]*([- ][A-Z][a-z]*)*", ErrorMessage = "Only letters, spaces and \"-\". First letters must be capital.")]
+        [Display(Name = "First Name")] //
         public string FirstName { get; set; }
 
         [Required]
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 1)]
+        //[RegularExpression(@"[A-Z][a-z]*([- ][A-Z][a-z]*)*", ErrorMessage = "Only letters, spaces and \"-\". First letters must be capital.")]
         [Display(Name = "Last Name")]
         public string LastName { get; set; }
 
-        [Required]
-        [StringLength(16, ErrorMessage = "The {0} must be {1} characters long", MinimumLength = 16)]
+        [Required] 
+        [RegularExpression(@"([A - Z]|[0-9]){16}", ErrorMessage = "Registration number consists of 16 capital letters and digits.")]
         [Display(Name = "Registration Number")]
         public string RegistrationNumber { get; set; }
 
