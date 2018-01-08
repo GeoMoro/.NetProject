5d70d7a28343b6798afb96067752f879095d71c5
Radu-Cristian Vulpescu <radu.vulpescu16@gmail.com>
2018-01-08 22:07:11 +0200

    Added project for unit tests for Lecture

---
+++ CourseManager/ServiceTests/LectureTests.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ServiceTests
+{
+    [TestClass]
+    public class LectureTests
+    {
+        [TestMethod]
+        public void Method_Input_ExpectedBehavior()
+        {
+            
+        }
+    }
+}
