359c0980274ff58f1dea2910bfd4c09a3e899a00
Geo <georgecosminmorosanu@gmail.com>
2018-01-16 21:18:23 +0200

    service?

---
+++ CourseManager/Data.Domain/Interfaces/ServicesInterfaces/IQuestionService.cs
@@ -0,0 +1,17 @@
+﻿using Data.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Data.Domain.Interfaces.ServicesInterfaces
+{
+    public interface IQuestionService
+    {
+        IReadOnlyList<Question> GetAllQuestions();
+        IList<Answer> GetAllAnswersForQuestion(Guid id);
+        Question GetQuestionById(Guid id);
+        void CreateQuestion(Question question);
+        void EditQuestion(Question question);
+        void DeleteQuestion(Question question);
+    }
+}
